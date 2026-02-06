92f52f229bd90bd82759bb6d1bc6fd9b0b3a0f8a
UmaairFaian <umairfaiz28@gmail.com>
2026-02-06 08:17:32 +0530

    cgange

--- src/App.tsx
+++ src/App.tsx
@@ -4,6 +4,7 @@ import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 import { BrowserRouter, Routes, Route, useLocation } from "react-router-dom";
 import { AnimatePresence } from "framer-motion";
+import ScrollToTop from "@/components/ScrollToTop";
 import Index from "./pages/Index";
 import CapabilitySystem from "./pages/CapabilitySystem";
 import PredictiveMaintenance from "./pages/PredictiveMaintenance";
@@ -37,6 +38,7 @@ const App = () => (
       <Toaster />
       <Sonner />
       <BrowserRouter>
+        <ScrollToTop />
         <AnimatedRoutes />
       </BrowserRouter>
     </TooltipProvider>
